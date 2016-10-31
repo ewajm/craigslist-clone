d836edc27bec9b30f8db3b79d84dfbca9555f89c
Ewa Manek and Katrina Hirsch <ewa@>
2016-10-31 14:44:53 -0700

    add functionality to delete a listing

--- app/routes/listing.js
+++ app/routes/listing.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('listing', params.listing_id);
+  },
+  actions: {
+    delete(listing) {
+      if (confirm('Delete this listing?')) {
+        listing.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
