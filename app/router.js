ea2dfbcba8233f4041bf65e2a8c12d8f0ee132f6
Ewa Manek and Katrina Hirsch <ewa@>
2016-10-31 15:24:49 -0700

    refactor category route to include category name

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('category', {path:"/category/:category_id"});
+  this.route('category', {path:"/category/:category_id/:category_name"});
   this.route('listing', {path:"/listing/:listing_id"});
 });
 
